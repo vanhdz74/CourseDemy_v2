d0b637ca2b97c2091ad4de1b8eed9edccb8e8845
Vanh <vanhnekdungso74@gmail.com>
2025-11-11 14:27:35 +0700

    chore(app): initialize Spring Boot main application class

---
+++ back-end/CourseWeb/src/main/java/com/vanh/CourseWeb/CourseWebApplication.java
@@ -0,0 +1,13 @@
+package com.vanh.CourseWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWebApplication.class, args);
+	}
+
+}
